6bca18998c2a08743fdc1e2d25b262af56cee881
jackschlehr <38955601+jackschlehr@users.noreply.github.com>
2021-08-04 03:00:41 -0400

    wsgi

--- djecommerce/settings/base.py
+++ djecommerce/settings/base.py
@@ -3,7 +3,6 @@ from decouple import config
 from django.core.management.utils import get_random_secret_key
 import sys
 import dj_database_url
-DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
 
 BASE_DIR = os.path.dirname(os.path.dirname(
     os.path.dirname(os.path.abspath(__file__))))
--- djecommerce/wsgi.py
+++ djecommerce/wsgi.py
@@ -2,7 +2,12 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'djecommerce.settings')
-
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+if DEVELOPMENT_MODE:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'djecommerce.settings.development')
+else:
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'djecommerce.settings.production')
 
 application = get_wsgi_application()
